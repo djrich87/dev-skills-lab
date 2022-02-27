2a700705d03a48d186331973d264b42b60ca19dd
djrich87 <djrich87@gmail.com>
2022-02-27 14:17:17 -0700

    commit

--- routes/skills.js
+++ routes/skills.js
@@ -8,6 +8,7 @@ router.get('/', skillCtrl.index)
 router.get('/new', skillCtrl.new)
 router.get('/:id', skillCtrl.show)
 router.post('/', skillCtrl.create)
+router.delete('/:id', skillCtrl.delete)
 
 
 export {
